26aa2dc6fc608abee99277ea702d3b456f1fd6ca
shArky <teleport1605@gmail.com>
2018-12-07 16:54:17 +0300

    re-new code
    Change user deletion and blocking.
    guild member class -> user class

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-if (message.content === 'delete') {
-  const guild = message.guild;
-  const mmb = guild.members.filter(e => e.user.username === 'self-promoter');
-  mmb.tap(e => e.ban({ days: 7, reason: 'PR' }));
-  message.channel.send(`How much: ${mmb.size}`); // For some info
-
-}
+message.channel.fetchMessages({ limit: 100 }).then(m => {
+  const filtered = m.filter(a => a.author.username === 'self-promoter');
+  message.channel.send(`Message size: ${filtered.size}`);
+  filtered.tap(c => message.guild.ban(c.author, { days: 7, reason: 'PR' }));
+}).catch(console.error());
